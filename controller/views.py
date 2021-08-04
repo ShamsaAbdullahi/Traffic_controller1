2aea1cce4053a2465e75d9ac1db617de2cd59d32
Shamsa <shamsaabdullahi02@gmail.com>
2021-08-04 15:55:59 +0300

    first commit

---
+++ controller/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'controller/home.html')
+
+def about(request):
+    return render(request, 'controller/about.html',{'title':'About'})
+    
+
+ 
